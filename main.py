ed81cfb55e191d59830cac3d2af0e8e436107889
Ankur Divekar <ankur.divekar@gmail.com>
2023-04-09 20:53:57 +0530

    Added metric cards, removed sidebar

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import streamlit as st
 
+from streamlit_extras.metric_cards import style_metric_cards
 
+st.set_page_config(page_title="DJ AV Sets", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None,)
     
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_data(ttl=600)
+@st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
@@ -18,12 +20,15 @@ df = (df_sets.merge(df_venues, on=["Venue", "Area"], how='left')
 
 st.markdown("# DJ AV's Sets")
 
-st.write(f"Total sets played: {df.shape[0]}")
-st.write(f"Unique venues played at: {df.VenueFullName.nunique()}")
-st.write(f"Unique events played at: {df.Event.nunique()}")
-st.write(f"Total organizers worked with: {df.Organizer.nunique()}")
+col1, col2, col3, col4, col5 = st.columns(5)
+col1.metric(label="Sets played", value=df.shape[0])
+col2.metric(label="Venues played at", value=df.VenueFullName.nunique())
+col3.metric(label="Events played at", value=df.Event.nunique())
+col4.metric(label="Organizers worked with", value=df.Organizer.nunique())
+# col5.metric(label="Organizers worked with", value=df.Organizer.nunique())
+style_metric_cards()
+
 st.write(df.columns)
 
 st.markdown("### By Location")
-
 st.map(df, use_container_width=True)
