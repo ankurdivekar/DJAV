f9cdb435273e1a4a8e1d847809223f6c13781f16
Ankur Divekar <ankur.divekar@gmail.com>
2023-04-09 20:09:25 +0530

    Basic map working

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ import streamlit as st
 
 def generate_invite():
     
-    pass
+    # Read in data from the Google Sheet.
+    # Uses st.cache_data to only rerun when the query changes or after 10 min.
+    # @st.cache_data(ttl=600)
+    def load_data(sheets_url):
+        csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+        return pd.read_csv(csv_url)
+
+    df_sets = load_data(st.secrets["sets_url"])
+    df_venues = load_data(st.secrets["venues_url"])
+    df = df_sets.merge(df_venues, on=["Venue", "Area"], how='left').rename(columns={"Latitude": 'latitude', "Longitude": 'longitude'})
+
+    st.markdown("# DJ AV's Sets")
+
+    st.markdown("### By Location")
+
+    st.map(df, use_container_width=True)
 
 
 st.sidebar.markdown("""---""")
