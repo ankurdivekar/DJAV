3683ad282ad4dd4c46a748ac5e58aae3d2739cc9
Ankur Divekar <ankur.divekar@gmail.com>
2023-04-09 20:25:39 +0530

    Added basic stats

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 import pandas as pd
 import streamlit as st
 
-def generate_invite():
+
     
-    # Read in data from the Google Sheet.
-    # Uses st.cache_data to only rerun when the query changes or after 10 min.
-    # @st.cache_data(ttl=600)
-    def load_data(sheets_url):
-        csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-        return pd.read_csv(csv_url)
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-    df_sets = load_data(st.secrets["sets_url"])
-    df_venues = load_data(st.secrets["venues_url"])
-    df = df_sets.merge(df_venues, on=["Venue", "Area"], how='left').rename(columns={"Latitude": 'latitude', "Longitude": 'longitude'})
+df_sets = load_data(st.secrets["sets_url"])
+df_venues = load_data(st.secrets["venues_url"])
+df = (df_sets.merge(df_venues, on=["Venue", "Area"], how='left')
+.rename(columns={"Latitude": 'latitude', "Longitude": 'longitude'})
+)
 
-    st.markdown("# DJ AV's Sets")
+st.markdown("# DJ AV's Sets")
 
-    st.markdown("### By Location")
+st.write(f"Total sets played: {df.shape[0]}")
+st.write(f"Unique venues played at: {df.VenueFullName.nunique()}")
+st.write(f"Unique events played at: {df.Event.nunique()}")
+st.write(f"Total organizers worked with: {df.Organizer.nunique()}")
+st.write(df.columns)
 
-    st.map(df, use_container_width=True)
+st.markdown("### By Location")
 
-
-st.sidebar.markdown("""---""")
-st.sidebar.title("DJ AV Sets")
-st.sidebar.markdown("Built with :heart: by [Ankur](https://instagram.com/raagarock)")
-st.sidebar.markdown("""---""")
-
-
-page_names_to_funcs = {
-    "Generate Invite": generate_invite,
-
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+st.map(df, use_container_width=True)
